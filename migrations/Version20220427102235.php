653d0cf2c581e84a755956e37e82e56cb917b008
user <andreyka.neklyudov@mail.ru>
2022-05-11 14:34:06 +0300

    веб8

--- migrations/Version20220427102235.php
+++ migrations/Version20220427102235.php
@@ -32,7 +32,7 @@ final class Version20220427102235 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE users (id INT NOT NULL, user_name VARCHAR(100) NOT NULL, user_surname VARCHAR(100) NOT NULL, user_email VARCHAR(200) NOT NULL, user_phone VARCHAR(14) NOT NULL, user_password VARCHAR(50) NOT NULL, is_admin BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE user (id INT NOT NULL, user_name VARCHAR(100) NOT NULL, user_surname VARCHAR(100) NOT NULL, user_email VARCHAR(200) NOT NULL, user_phone VARCHAR(14) NOT NULL, user_password VARCHAR(50) NOT NULL, is_admin BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE "user"');
     }
 }
